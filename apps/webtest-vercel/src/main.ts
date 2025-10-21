9a306cf6b650625b98b567d1c2363b195ed64bbe
Kristian Hafell <146472912+KristianHafell@users.noreply.github.com>
2025-10-21 10:41:50 +0200

    test website vercel

--- apps/webtest-vercel/src/main.ts
+++ apps/webtest-vercel/src/main.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (_, res) => res.send('Hello from Node + Nx + Vercel!'));
+app.get('/', (_, res) => res.send('from Node + Nx + Vercel!'));
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => console.log(`Server running on port ${port}`));
