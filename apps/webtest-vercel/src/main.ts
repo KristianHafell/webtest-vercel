eed212398116aeee3c842eb58160de8d3a271248
Kristian Hafell <146472912+KristianHafell@users.noreply.github.com>
2025-10-21 10:00:15 +0200

    init

--- apps/webtest-vercel/src/main.ts
+++ apps/webtest-vercel/src/main.ts
@@ -1,1 +1,8 @@
-console.log('Hello World');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_, res) => res.send('Hello from Node + Nx + Vercel!'));
+
+const port = process.env.PORT || 3002;
+app.listen(port, () => console.log(`Server running on port ${port}`));
